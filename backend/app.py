be98cf122148b638b4d76cfdbfac80ba2102b122
travyozza <84102421+travyozza@users.noreply.github.com>
2024-11-21 14:33:33 +1300

    get by team added

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,8 @@ def connectDB():
     conn = psycopg2.connect(url)
     return conn
 
+team_ids = {'Sharks': 500028, 'Storm': 500021, 'Panthers': 500014, 'Broncos': 500011, 'Sea Eagles': 500002, 'Warriors': 500032, 'Knights': 500003, 'Cowboys': 500012, 'Bulldogs': 500010, 'Rabbitohs': 500005, 'Roosters': 500001, 'Tigers': 500023, 'Dragons': 500022, 'Titans': 500004, 'Raiders': 500013, 'Eels': 500031, 'Dolphins': 500723}
+
 app = Flask(__name__)
 
 @app.get("/")
@@ -41,8 +43,6 @@ def playerinfo(id):
 
 @app.route("/api/data/playerinfo/<first_name>-<last_name>", methods=["GET"])
 def playerInfo_byName(first_name, last_name):
-    #first_name = first_name.capitalize()
-    #last_name = last_name.capitalize()
     conn = connectDB()
     cursor = conn.cursor(cursor_factory=RealDictCursor)
     
@@ -54,6 +54,28 @@ def playerInfo_byName(first_name, last_name):
             return jsonify({"error": "Row not found"}), 404
 
         return jsonify(row), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route("/api/data/team/<team_name>", methods=["GET"])
+def getTeams(team_name):
+    team_name = team_name.capitalize()
+    conn = connectDB()
+    cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+    try:
+        cursor.execute("SELECT * FROM main WHERE squad_id = %s", (team_ids[team_name],))
+        rows = cursor.fetchall()
+
+        if rows is None:
+            return jsonify({"error": "Team not found"}), 404
+
+        return jsonify(rows), 200
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
